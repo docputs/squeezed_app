13d30d0ef2ba209016b7b863a8807b353b65859a
João Vitor <docputs@gmail.com>
2021-09-02 12:33:11 -0300

    add user creation trigger on functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,9 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const createUserAfterRegistration = functions.auth.user()
+    .onCreate((user, _) => {
+      return admin.firestore().collection("users").doc(user.uid).create({});
+    });
